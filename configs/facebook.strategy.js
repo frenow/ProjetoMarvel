1487e1686c106c23d28f3c65f9437dd12d3a1da3
unknown <frenow@gmail.com>
2019-06-02 15:07:25 -0300

    alterado strategy facebook para heroku

--- configs/facebook.strategy.js
+++ configs/facebook.strategy.js
@@ -3,14 +3,14 @@ const FaceBookbStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FaceBookbStrategy({
     //configs para localhost
-    clientID: "449338872490395",
-    clientSecret: "b25ba39f754668f409406315e54a1776",
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    //clientID: "449338872490395",
+    //clientSecret: "b25ba39f754668f409406315e54a1776",
+    //callbackURL: "http://localhost:3000/auth/facebook/callback",
     
     //configs para heroku
-    //clientID: "649576658802936",
-    //clientSecret: "6578e0add51c02af7c0252227e678771",
-    //callbackURL: "https://projetomarvel.herokuapp.com/auth/facebook/callback",
+    clientID: "649576658802936",
+    clientSecret: "6578e0add51c02af7c0252227e678771",
+    callbackURL: "https://projetomarvel.herokuapp.com/auth/facebook/callback",
     enableProof: true
 },
 function(accessToken, refreshToken, profile, cb) {
